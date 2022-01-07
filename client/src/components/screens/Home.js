d5c3dcd3aaad551720be3470944bfb7217e964e9
rhinoxD <coolshiv0721@gmail.com>
2022-01-07 19:15:01 +0530

    Home Page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,8 +2,55 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='home'>
+      <div className='card home-card'>
+        <h5>Laura</h5>
+        <div className='card-image'>
+          <img
+            src='https://tripspi-prod.imgix.net/blogs/60ewipugMR/Pfiv0-david-edelstein-N4DbvTUDikw-unsplash.jpg?ixlib=js-2.3.2&w=1024&auto=compress&fit=crop&s=919afc22d09ca569980b1c99fd2af23d'
+            alt=''
+          />
+        </div>
+        <div className='card-content'>
+          <i class='material-icons' style={{ color: 'red' }}>
+            favorite
+          </i>
+          <h6>Title</h6>
+          <p>This is an Amazing place </p>
+          <input type='text' placeholder='Add a comment' />
+        </div>
+      </div>
+      <div className='card home-card'>
+        <h5>Laura</h5>
+        <div className='card-image'>
+          <img src='https://wallpaperaccess.com/full/18448.jpg' alt='' />
+        </div>
+        <div className='card-content'>
+          <i class='material-icons' style={{ color: 'red' }}>
+            favorite
+          </i>
+          <h6>Title</h6>
+          <p>This is an Amazing place </p>
+          <input type='text' placeholder='Add a comment' />
+        </div>
+      </div>
+      <div className='card home-card'>
+        <h5>Laura</h5>
+        <div className='card-image'>
+          <img
+            src='https://tripspi-prod.imgix.net/blogs/60ewipugMR/Pfiv0-david-edelstein-N4DbvTUDikw-unsplash.jpg?ixlib=js-2.3.2&w=1024&auto=compress&fit=crop&s=919afc22d09ca569980b1c99fd2af23d'
+            alt=''
+          />
+        </div>
+        <div className='card-content'>
+          <i class='material-icons' style={{ color: 'red' }}>
+            favorite
+          </i>
+          <h6>Ass</h6>
+          <p>OH MY GOD! </p>
+          <input type='text' placeholder='Add a comment' />
+        </div>
+      </div>
     </div>
   );
 };
