32c96745941d8677a6197c9206f5154f010ebdd0
rhinoxD <coolshiv0721@gmail.com>
2022-01-07 18:35:08 +0530

    Creating SignIn and Sign Up Page UI

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signin = () => {
   return (
-    <div>
-      <h1>Signin</h1>
+    <div className='my-card'>
+      <div className='card auth-card input-field'>
+        <h2>Instagram</h2>
+        <input type='text' placeholder='Email' />
+        <input type='password' placeholder='Password' />
+        <button className='btn waves-effect waves-light #2196f3 blue'>
+          Login
+        </button>
+        <h6>
+          <Link to='/signup'>Don't have an account?</Link>
+        </h6>
+      </div>
     </div>
   );
 };
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className='my-card'>
+      <div className='card auth-card input-field'>
+        <h2>Instagram</h2>
+        <input type='text' placeholder='Name' />
+        <input type='email' placeholder='Email' />
+        <input type='password' placeholder='Password' />
+        <button className='btn waves-effect waves-light #2196f3 blue'>
+          Sign Up
+        </button>
+        <h6>
+          <Link to='/signin'>Already have an account?</Link>
+        </h6>
+      </div>
     </div>
   );
 };
