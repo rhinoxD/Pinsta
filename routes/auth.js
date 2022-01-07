1823462150e71bad7408332ac6e487ed50c86b4b
rhinoxD <coolshiv0721@gmail.com>
2022-01-07 10:33:51 +0530

    Hashing Passwords

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const bcrypt = require('bcryptjs');
 
 router.post('/signup', async (req, res) => {
   const { name, email, password } = req.body;
@@ -17,13 +18,16 @@ router.post('/signup', async (req, res) => {
         .status(422)
         .json({ error: 'User already exists with this email.' });
     }
-    const user = new User({
-      name,
-      email,
-      password,
-    });
-    await user.save();
-    res.json({ message: 'Registered Successfully.' });
+    const hashedPassword = await bcrypt.hash(password, 12);
+    if (hashedPassword) {
+      const user = new User({
+        name,
+        email,
+        password: hashedPassword,
+      });
+      await user.save();
+      res.json({ message: 'Registered Successfully.' });
+    }
   } catch (error) {
     console.log(error);
   }
