e94806c58ba73b4d81ac41895e22a22db009cb8e
rhinoxD <coolshiv0721@gmail.com>
2022-01-07 10:26:02 +0530

    Posting Data to Database

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     return res.status(422).json({
       error: 'Please fill the required fields.',
     });
   }
-  res.status(201).json({
-    message: 'Registered.',
-  });
+  try {
+    const savedUser = await User.findOne({ email });
+    if (savedUser) {
+      return res
+        .status(422)
+        .json({ error: 'User already exists with this email.' });
+    }
+    const user = new User({
+      name,
+      email,
+      password,
+    });
+    await user.save();
+    res.json({ message: 'Registered Successfully.' });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
