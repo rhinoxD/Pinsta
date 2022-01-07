93f3ede0a2594302db3a70b7cbec45f3454d677b
rhinoxD <coolshiv0721@gmail.com>
2022-01-07 09:49:27 +0530

    Setting Up Mongodb on Atlas

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const PORT = 5000;
+const { MONGOURI } = require('./keys');
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+mongoose.connect(MONGOURI);
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Mongo');
+});
+mongoose.connection.on('error', (err) => {
+  console.log('Error Connecting', err);
 });
 
 app.listen(PORT, () => {
---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI:
+    'mongodb+srv://user:4dnpZXQhXK0wJjZk@cluster0.z398l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+};
