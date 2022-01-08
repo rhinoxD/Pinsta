ea337f778c6036747ff1f83c46d18189698c31e6
rhinoxD <coolshiv0721@gmail.com>
2022-01-08 14:25:17 +0530

    Showing Details of Logged In User in Profile Page

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { UserContext } from '../../App';
 
 const Profile = () => {
+  const [mypics, setMypics] = useState([]);
+  const { state, dispatch } = useContext(UserContext);
+  useEffect(() => {
+    fetch('/myposts', {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setMypics(result.myPosts);
+      });
+  }, []);
   return (
     <div style={{ maxWidth: '750px', margin: '0 auto' }}>
       <div
@@ -20,7 +34,7 @@ const Profile = () => {
           />
         </div>
         <div>
-          <h5>Laura Wilson</h5>
+          <h5>{state && state.payload.name}</h5>
           <div
             style={{
               display: 'flex',
@@ -35,41 +49,16 @@ const Profile = () => {
         </div>
       </div>
       <div className='gallery'>
-        <img
-          className='item'
-          src='https://yt3.ggpht.com/VZdyGhjcWxPkljggdCqqj8sK8Xcy5NdrlJbgbwn0BD5hFdjz0NScRknEqPHEeECHj0I4iGqpPA=s900-c-k-c0x00ffffff-no-rj'
-          alt=''
-        />
-        <img
-          className='item'
-          src='https://yt3.ggpht.com/VZdyGhjcWxPkljggdCqqj8sK8Xcy5NdrlJbgbwn0BD5hFdjz0NScRknEqPHEeECHj0I4iGqpPA=s900-c-k-c0x00ffffff-no-rj'
-          alt=''
-        />
-        <img
-          className='item'
-          src='https://yt3.ggpht.com/VZdyGhjcWxPkljggdCqqj8sK8Xcy5NdrlJbgbwn0BD5hFdjz0NScRknEqPHEeECHj0I4iGqpPA=s900-c-k-c0x00ffffff-no-rj'
-          alt=''
-        />
-        <img
-          className='item'
-          src='https://yt3.ggpht.com/VZdyGhjcWxPkljggdCqqj8sK8Xcy5NdrlJbgbwn0BD5hFdjz0NScRknEqPHEeECHj0I4iGqpPA=s900-c-k-c0x00ffffff-no-rj'
-          alt=''
-        />
-        <img
-          className='item'
-          src='https://yt3.ggpht.com/VZdyGhjcWxPkljggdCqqj8sK8Xcy5NdrlJbgbwn0BD5hFdjz0NScRknEqPHEeECHj0I4iGqpPA=s900-c-k-c0x00ffffff-no-rj'
-          alt=''
-        />
-        <img
-          className='item'
-          src='https://yt3.ggpht.com/VZdyGhjcWxPkljggdCqqj8sK8Xcy5NdrlJbgbwn0BD5hFdjz0NScRknEqPHEeECHj0I4iGqpPA=s900-c-k-c0x00ffffff-no-rj'
-          alt=''
-        />
-        <img
-          className='item'
-          src='https://yt3.ggpht.com/VZdyGhjcWxPkljggdCqqj8sK8Xcy5NdrlJbgbwn0BD5hFdjz0NScRknEqPHEeECHj0I4iGqpPA=s900-c-k-c0x00ffffff-no-rj'
-          alt=''
-        />
+        {mypics.map((item) => {
+          return (
+            <img
+              key={item._id}
+              className='item'
+              src={item.photo}
+              alt={item.title}
+            />
+          );
+        })}
       </div>
     </div>
   );
