a85b1b155f7143d80633e8a2cc062a1d3956487f
rhinoxD <coolshiv0721@gmail.com>
2022-01-08 12:06:21 +0530

    Adding Context API

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useEffect, createContext, useReducer, useContext } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/screens/Home';
@@ -6,17 +12,42 @@ import Profile from './components/screens/Profile';
 import Signin from './components/screens/Signin';
 import Signup from './components/screens/Signup';
 import CreatePost from './components/screens/CreatePost';
+import { reducer, initialState } from './reducers/userReducer';
 
-function App() {
+export const UserContext = createContext();
+
+const Routing = () => {
+  const history = useHistory();
+  const { state, dispatch } = useContext(UserContext);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      dispatch({ type: 'USER', payload: user });
+      history.push('/');
+    } else {
+      history.push('/signin');
+    }
+  }, []);
   return (
-    <Router>
-      <Navbar />
+    <Switch>
       <Route path='/' component={Home} exact />
       <Route path='/profile' component={Profile} exact />
       <Route path='/signin' component={Signin} exact />
       <Route path='/signup' component={Signup} exact />
       <Route path='/create' component={CreatePost} exact />
-    </Router>
+    </Switch>
+  );
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <UserContext.Provider value={{ state, dispatch }}>
+      <Router>
+        <Navbar />
+        <Routing />
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import { UserContext } from '../../App';
 import M from 'materialize-css';
 
 const Signin = () => {
+  const { state, dispatch } = useContext(UserContext);
   const history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -32,6 +34,7 @@ const Signin = () => {
         } else {
           localStorage.setItem('jwt', data.token);
           localStorage.setItem('user', JSON.stringify(data.user));
+          dispatch({ type: 'USER', payload: data.user });
           M.toast({ html: 'Logged In Successfully', classes: '#43a047 green' });
           history.push('/');
         }
