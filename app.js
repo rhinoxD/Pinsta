f701f0d70158b279a190701b138f65d9fe678ef6
rhinoxD <coolshiv0721@gmail.com>
2022-01-08 10:11:08 +0530

    User Signup by Posting Data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const PORT = 5000;
 const { MONGOURI } = require('./keys');
 
@@ -16,6 +17,7 @@ require('./models/user');
 require('./models/post');
 
 app.use(express.json());
+app.use(cors());
 app.use(require('./routes/auth'));
 app.use(require('./routes/post'));
 
