c96872c5bcff798fcdcc8aa17589c80c9ac62719
rhinoxD <coolshiv0721@gmail.com>
2022-01-08 12:28:29 +0530

    Changing Navigation Links after Signin and Signout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../App';
 
 const Navbar = () => {
-  return (
-    <nav>
-      <div className='nav-wrapper white'>
-        <Link to='/' className='brand-logo left'>
-          Instagram
-        </Link>
-        <ul id='nav-mobile' className='right'>
-          <li>
-            <Link to='/signin'>Login</Link>
-          </li>
-          <li>
-            <Link to='/signup'>Signup</Link>
-          </li>
+  const { state, dispatch } = useContext(UserContext);
+  const renderList = () => {
+    if (state) {
+      return [
+        <>
           <li>
             <Link to='/profile'>Profile</Link>
           </li>
           <li>
             <Link to='/create'>Create Post</Link>
           </li>
+        </>,
+      ];
+    } else {
+      return [
+        <>
+          <li>
+            <Link to='/signin'>Login</Link>
+          </li>
+          <li>
+            <Link to='/signup'>Signup</Link>
+          </li>
+        </>,
+      ];
+    }
+  };
+  return (
+    <nav>
+      <div className='nav-wrapper white'>
+        <Link to={state ? '/' : '/signin'} className='brand-logo left'>
+          Instagram
+        </Link>
+        <ul id='nav-mobile' className='right'>
+          {renderList()}
         </ul>
       </div>
     </nav>
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -3,7 +3,7 @@ export const initialState = null;
 export const reducer = (state, action) => {
   const { type, payload } = action;
   switch (type) {
-    case 'USERS':
+    case 'USER':
       return {
         payload,
       };
